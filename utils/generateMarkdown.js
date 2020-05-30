ef14c4d06fa53b3f8a7be8759de6e920f602dc08
dianaz <stingray2091@gmail.com>
2020-05-30 19:31:53 -0400

    finished format

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,24 +2,24 @@ function generateMarkdown(data) {
   return `
   [![Generic badge](https://img.shields.io/badge/Maintained-yes-blue)](https://shields.io/)
   
-  # User ID
-  ## ${data.userName}
-  # Name of Project
-  ## ${data.title}
-  # Description
-  ## ${data.Description}
-  # Installation
-  ## ${data.Installation}
-  # Usage
-  ## ${data.usage}
-  # License
-  ## ${data.license}
-  # Contributors
-  ## ${data.contributing}
-  # Tests
-  ## ${data.tests}
-  # Contact
-  ## ${data.questions}
+  # ${data.title} 
+  
+  ## User ID
+  ### ${data.userName}
+  ## Description
+  ### ${data.description}
+  ## Installation
+  ### ${data.Installation}
+  ## Usage
+  ### ${data.usage}
+  ## License
+  ### ${data.license}
+  ## Contributors
+  ### ${data.contributing}
+  ## Tests
+  ### ${data.tests}
+  ## Contact
+  ### ${data.questions}
   `;
 }
 
